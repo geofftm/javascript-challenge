1a69408a4a127a0d05b9e04139bc4b71e8be77d6
Geoff Matis <gmatis@gmail.com>
2021-02-21 11:11:34 -0500

    arrow function table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -20,11 +20,33 @@ var tbody = d3.select("tbody");
 // });
 
 // Step 3:  Use `Object.entries` to console.log each weather report value
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
-  var row = tbody.append("tr");
+// data.forEach(function(ufoReport) {
+//   console.log(ufoReport);
+//   var row = tbody.append("tr");
 
-  Object.entries(ufoReport).forEach(function([key, value]) {
-    console.log(key, value);
+//   Object.entries(ufoReport).forEach(function([key, value]) {
+//     console.log(key, value);
+//   });
+// });
+
+// Step 5: Use d3 to update each cell's text with
+// weather report values (weekday, date, high, low)
+// data.forEach(function(ufoReport) {
+//   console.log(ufoReport);
+//   var row = tbody.append("tr");
+//   Object.entries(ufoReport).forEach(function([key, value]) {
+//     console.log(key, value);
+//     // Append a cell to the row for each value
+//     // in the weather report object
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
+
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
   });
-});
