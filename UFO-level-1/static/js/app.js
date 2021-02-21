7dc9a03a9d8fd7ad3515d7bfe004b52dcbbb1f1a
Geoff Matis <gmatis@gmail.com>
2021-02-21 11:30:39 -0500

    filtered data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -50,3 +50,35 @@ data.forEach((ufoReport) => {
       cell.text(value);
     });
   });
+
+// Getting a reference to the button on the page with the id property set to `click-me`
+var button = d3.select("#filter-btn");
+
+// Getting a reference to the input element on the page with the id property set to 'input-field'
+var inputField = d3.select("#form-control");
+
+button.on("click", runEnter);
+
+
+// Create the function to run when the button is clicked
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Print the value to the console
+  console.log(inputValue);
+
+  var filteredData = tableData.filter(report => report.datetime === inputValue);
+
+  console.log(filteredData);
+
+
+
+}
